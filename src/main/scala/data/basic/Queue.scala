7e38aefeac5f630e14d00010444316a8c35cadd4
gleb <parakhonskiyg@gmail.com>
2019-11-03 21:48:54 +0100

    better implementation of Queue

--- src/main/scala/data/basic/Queue.scala
+++ src/main/scala/data/basic/Queue.scala
@@ -4,24 +4,33 @@ import List.nil
 
 /**
  * invariant: front is empty only if rear is empty
+ * #snoc, #isEmpty takes O(1) time. #tail takes O(1) amortised time.
  */
 class Queue[A](private val front: List[A], private val rear: List[A]) {
+  import Queue.queue
+
   def isEmpty: Boolean = front.isEmpty && rear.isEmpty
 
-  def snoc(a: A): Queue[A] = front.cata(new Queue(List(a), List.nil), _  => new Queue(front, a :: rear))
+  // enqueue the value
+  def snoc(a: A): Queue[A] = queue(front, a :: rear)
 
+  // dequeue the value
   def uncons: (Option[A], Queue[A]) =
     front.cata(
       None -> this,
-      cons => (Some(cons.head) -> cons.tail.cata(
-        new Queue(rear.reverse, nil),
-        xss => new Queue(xss, rear)))
+      cons => Some(cons.head) -> queue(cons.tail, rear)
     )
 
   def tail: Queue[A] = uncons._2
+
 }
 
 object Queue {
   def empty[A] = new Queue(List.empty, List.empty)
+
+  private[basic] def queue[A](f: List[A], r: List[A]): Queue[A] = f.cata(
+    new Queue(r.reverse, List.nil),
+    _ => new Queue(f, r)
+  )
 
 }
