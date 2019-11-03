0291d2c32164c5f18346b4aa56fc8452febca256
gleb <parakhonskiyg@gmail.com>
2019-11-03 19:55:53 +0100

    compile

--- src/main/scala/data/basic/List.scala
+++ src/main/scala/data/basic/List.scala
@@ -3,6 +3,13 @@ package data.basic
 import scala.annotation.tailrec
 
 sealed abstract class List[A] {
+  def isEmpty: Boolean = cata(true, _ => false)
+
+  def cata[B](caseNil: => B, caseCons: Cons[A] => B): B = this match {
+    case c@Cons(head, tail) => caseCons(c)
+    case _: X.type => caseNil
+  }
+
   def ++(list: List[A]): List[A] = {
     def go(dec: List[A], res: List[A]): List[A] = dec match {
       case h Cons t => go(t, h :: res)
@@ -15,14 +22,26 @@ sealed abstract class List[A] {
 
   def reverse: List[A] = List.reverse(this)
 
+  def uncons: (Option[A], List[A]) = cata(None -> List.empty, c => Some(c.head) -> c.tail)
+
 }
+
+/**
+  * trick with object X is to avoid invariance and use the same object for each empty List
+  */
 case class Cons[A](head: A, tail: List[A]) extends List[A]
 case object X                              extends List[A forSome { type A }]
 
 object List extends ListInstances {
 
+  def apply[A](a: A*): List[A] = {
+    fromScala(a.toList)    
+  }
+
   def fromScala[A](l: scala.collection.immutable.List[A]): List[A] =
     l.foldLeft(nil[A])((a, b) => b :: a).reverse
+
+  def empty[A]: List[A] = nil[A]
 
   def nil[A]: List[A] = X.asInstanceOf[List[A]]
 
